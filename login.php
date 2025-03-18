a3d76053489e2e5c47c13bb070208d15fad5e252
anthony <cfanthonyalexandrerodriguescavalcanti@torreroja.cat>
2025-03-18 16:09:56 +0100

    Formulario Login php.

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
-hola
+<form action="login.php" method="post">
+    <label>Usuario:</label>
+    <input type="text" name="usuario" required>
+    
+    <label>Contraseña:</label>
+    <input type="password" name="contraseña" required>
+    
+    <button type="submit">Iniciar sesión</button>
+    <input type="checkbox" name="recordar"> Recordarme
+    
+    <button type="button">Cancelar</button>
+    <a href="#">Olvidé mi contraseña</a>
+</form>
